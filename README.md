d430cfe882ace9744ab72fdb84db40a91c46e848
Maystern <12112910@mail.sustech.edu.cn>
2023-12-26 22:07:24 +0800

    first commit

--- README.md
+++ README.md
@@ -1,3 +1,134 @@
 # WheelRadiusPointCloud
 
-This project is private and used for backing up code.
+Yufan Xiao, Jiacheng Luo
+
+## Project Introduction
+
+This project is based on the 3D point cloud of a grinding wheel modeled as shown in the figure below, aiming to estimate its radius.
+
+## Algorithm Description
+The algorithm aggregates the data near the y-axis through mean pooling, transforming it into a 2D plane problem and obtaining several batches. For each batch, it uses downsampling, DBSCAN clustering, and least squares fitting to obtain the boundary equations of the two straight lines on both sides. Then, using the original data on the angle bisector, it estimates the circle through gradient descent using the MSE loss function. Finally, for each batch of data, it reports the estimated radius of the circle.
+
+
+## How to Use
+
+First, use `conda` to install a virtual environment named `corfit` and configure all the required packages for the code.
+
+```
+(base) $ conda create -n corfit python=3.7.16
+(base) $ conda activate corfit
+(corfit) $ pip install -r requirements.txt
+```
+
+Check if the project is configured in the following format:
+
+```
+.
+├── data
+├── pic
+├── results
+├── README.md
+├── main.py
+├── help.ipynb
+└── requirements.txt
+```
+
+Place the raw data files in CSV format that need to be processed in the `./data` directory, such as `sample.csv` (you can download the sample file by clicking: [download](https://share.weiyun.com/1Azqvb3Y)).
+
+The format of the `sample.csv` file should be as follows, including headers, the number of points, and data separated by spaces:
+
+```
+//X Y Z
+270385
+4.28700018 0.00000000 -0.21818000
+4.28999996 0.00000000 -0.21731000
+4.29300022 0.00000000 -0.21613000
+4.29600000 0.00000000 -0.21573000
+4.29899979 0.00000000 -0.21510001
+4.30200005 0.00000000 -0.21681000
+4.38600016 0.00000000 -0.11171000
+4.38899994 0.00000000 -0.10954000
+4.39200020 0.00000000 -0.10778000
+4.39499998 0.00000000 -0.10637000
+4.39799976 0.00000000 -0.10401000
+...
+```
+
+Next, you can use visualization tools like CloudCompare to load and inspect the processed point cloud data.
+
+<img src="https://raw.githubusercontent.com/Maystern/picbed/main/image-20231226205904746.png" alt="Your Image" width="400"/>
+
+
+To run the code, use the following command: `python main.py -n sample.csv` (where the `-n` parameter is mandatory, and other parameters have default values that can be modified). For detailed information on other parameters, you can refer to `python main.py --help`.
+
+```
+(corfit) $ python main.py -help
+usage: main.py [-h] [-n CORNER_EXAMPLE_NAME] [-pic PIC_STORE_NAME]
+               [-result RESULT_STORE_NAME] [-i EXPECTED_INTERVAL_COUNT]
+               [-d DOWNSAMPLING_FACTOR] [-w LINE_FITTING_WINDOW_SIZE]
+               [-s RANDOM_SEED] [-dbcan_eps DBSCAN_EPS]
+               [-dbcan_min_sample DBSCAN_MIN_SAMPLE] [-l LEARNING_RATE]
+               [-e EPSILON] [-r PRIOR_CIRCLE_R]
+
+Estimation of Grinding Wheel Radius Using Gradient Descent Algorithm Based on
+CSV Point Cloud Data.
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -n CORNER_EXAMPLE_NAME
+                        source data CSV file name, which should be placed in
+                        the ./data folder.
+  -pic PIC_STORE_NAME   output image folder name, which should be placed in
+                        the project root directory ./
+  -result RESULT_STORE_NAME
+                        output results folder name, which should be placed in
+                        the root directory ./
+  -i EXPECTED_INTERVAL_COUNT
+                        aggregation factor for y, how many neighboring y
+                        values to calculate R at a time.
+  -d DOWNSAMPLING_FACTOR
+                        downsampling factor, determining a point for
+                        estimating the lines on both sides every few data
+                        points.
+  -w LINE_FITTING_WINDOW_SIZE
+                        number of points in the sliding window, consecutively
+                        fitting lines with multiple downsampling points to
+                        calculate the normal vector.
+  -s RANDOM_SEED        random seed
+  -dbcan_eps DBSCAN_EPS
+                        radius size of the neighborhood for DBSCAN.
+  -dbcan_min_sample DBSCAN_MIN_SAMPLE
+                        minimum number of members in a cluster for DBSCAN.
+  -l LEARNING_RATE      learning rate for gradient descent using Adam.
+  -e EPSILON            Termination criterion for gradient descent, |loss' -
+                        loss| < epsilon
+  -r PRIOR_CIRCLE_R     Prior circle radius, which is used to set the initial
+                        value for gradient descent in estimating the original
+                        radius of the grinding wheel.
+```
+
+
+You can view the results of DBSCAN clustering and the fitting calculation for this batch in the `./pic/sample/batch_id` directory, as shown in the following images:
+
+<img src="https://raw.githubusercontent.com/Maystern/picbed/main/DBSCAN_clustering_result.png" alt="DBSCAN_clustering_result" width="400"/> <img src="https://raw.githubusercontent.com/Maystern/picbed/main/new_scatter_plot——1.png" alt="new_scatter_plot——1" width="400"/>
+
+
+Once the progress bar completes, you will find all batch fitting results (radius, angle between fitted lines) and a visualized histogram in the `./results/sample` folder, as illustrated below:
+```
+// results.csv
+Block,R,Angle
+1,0.11148012358179621,89.90166948614373
+2,0.1003519830657477,89.79184732861077
+3,0.10538032984418592,89.55709871217091
+4,0.1024437819096742,89.9580235832247
+5,0.11876593782266195,89.8372186694736
+6,0.10789719614089213,89.79062547012468
+7,0.10562980246049125,89.67696872640295
+8,0.11557501790091716,89.55541525935509
+9,0.11232309985505863,89.9113023350095
+10,0.11259368715129213,90.22478354468474
+11,0.09691442408830694,89.62875472888935
+...
+```
+
+![djashdjkashdkja](https://raw.githubusercontent.com/Maystern/picbed/main/djashdjkashdkja.png)
--- main.py
+++ main.py
@@ -1,51 +1,61 @@
 import csv
-from itertools import islice
 import numpy as np
 from tqdm import tqdm
 import os
 import matplotlib.pyplot as plt
-from sklearn.cluster import KMeans
-from sklearn.metrics import silhouette_score
 from sklearn.cluster import DBSCAN
 from collections import Counter
 import torch
 import torch.optim as optim
+import seaborn as sns
+import argparse
 
+def parse_args():    
+    
+    parser = argparse.ArgumentParser(description='Estimation of Grinding Wheel Radius Using Gradient Descent Algorithm Based on CSV Point Cloud Data.')
+    parser.add_argument('-n', dest='corner_example_name', type=str, default= None, # csv文件名称, 需要放在./data文件夹下
+                        help='source data CSV file name, which should be placed in the ./data folder.') 
+    parser.add_argument('-pic', dest='pic_store_name', type=str, default='pic', # 存储输出图片文件夹，需要放在项目根目录./下
+                        help='output image folder name, which should be placed in the project root directory ./')
+    parser.add_argument('-result', dest='result_store_name', type=str, default='results', # 存储输出结果文件夹，需要放在根目录./下
+                        help='output results folder name, which should be placed in the root directory ./')
+    parser.add_argument('-i', dest='expected_interval_count', type=int, default=10, # 对 y 汇聚因数, 邻近多少个y计算一次R
+                        help='aggregation factor for y, how many neighboring y values to calculate R at a time.')
+    parser.add_argument('-d', dest='downsampling_factor', type=int, default=5, # 下采样因数
+                        help='downsampling factor, determining a point for estimating the lines on both sides every few data points.')
+    parser.add_argument('-w', dest='line_fitting_window_size', type=int, default=5, # 滑动窗口点数
+                        help='number of points in the sliding window, consecutively fitting lines with multiple downsampling points to calculate the normal vector.')
+    parser.add_argument('-s', dest='random_seed', type=int, default=42, # 随机种子
+                        help='random seed')
+    parser.add_argument('-dbcan_eps', dest='dbscan_eps', type=float, default=0.05, # dbscan 邻域的半径大小
+                        help='radius size of the neighborhood for DBSCAN.')
+    parser.add_argument('-dbcan_min_sample', dest='dbscan_min_sample', type=int, default=5, # dbscan 簇最小成员数
+                        help='minimum number of members in a cluster for DBSCAN.')
+    parser.add_argument('-l', dest='learning_rate', type=float, default=0.01, # 梯度下降的学习率
+                        help='learning rate for gradient descent using Adam.')
+    parser.add_argument('-e', dest='epsilon', type=float, default=1e-8, # 梯度下降的终止条件
+                        help='Termination criterion for gradient descent, |loss\' - loss| < epsilon')
+    parser.add_argument('-r', dest='prior_circle_r', type=float, default=1, # 先验的圆半径
+                        help='Prior circle radius, which is used to set the initial value for gradient descent in estimating the original radius of the grinding wheel.')
+    args = parser.parse_args()
+    
+    assert (args.corner_example_name is not None), "Please utilize the command \'python main.py -n sample.csv\' to specify the source data CSV file." 
+    
+    return args
 
-# 设置区域 ---------- begin ----------
-
-"""
-corner_example_name = "clear_cloud_for_test" # csv文件名称, 需要放在./data文件夹下
-pic_store_name = "pic" # 存储输出图片文件夹，需要放在项目根目录./下
-result_store_name = "results" # 存储输出结果文件夹，需要放在根目录./下
-expected_interval_count = 50 # 对 y 汇聚因数, 邻近多少个y计算一次R
-downsampling_factor = 5 # 下采样因数
-line_fitting_window_size = 5 # 滑动窗口点数
-random_seed = 42 # 随机种子
-dbscan_eps = 0.05 # dbscan 邻域的半径大小
-dbscan_min_sample = 5 # dbscan 簇最小成员数
-learning_rate = 1e-2 # 梯度下降的学习率
-num_iterations = 200 # 梯度下降执行次数
-prior_circle_dis = 1 # 先验的圆半径
-"""
-
-
-corner_example_name = "200r2"
-pic_store_name = "pic"
-result_store_name = "results"
-expected_interval_count = 50
-downsampling_factor = 5
-line_fitting_window_size = 5
-random_seed = 42
-dbscan_eps = 0.01
-dbscan_min_sample = 5
-learning_rate = 0.01
-num_iterations = 200
-prior_circle_dis = 1
-
-
-
-# 设置区域 ---------- end ----------
+args = parse_args()
+corner_example_name = args.corner_example_name[:-4]
+pic_store_name = args.pic_store_name
+result_store_name = args.result_store_name
+expected_interval_count = args.expected_interval_count
+downsampling_factor = args.downsampling_factor
+line_fitting_window_size = args.line_fitting_window_size
+random_seed = args.random_seed
+dbscan_eps = args.dbscan_eps
+dbscan_min_sample = args.dbscan_min_sample
+learning_rate = args.learning_rate
+epsilon = args.epsilon
+prior_circle_r =  args.prior_circle_r
 
 store_pic_folder_name = os.path.join( "./" + pic_store_name, corner_example_name)
 store_result_folder_name = os.path.join("./" + result_store_name, corner_example_name)
@@ -67,8 +77,8 @@ with open(os.path.join(store_result_folder_name, "settings.txt"), 'w') as file:
     file.write(f"dbscan_eps = {dbscan_eps}\n")
     file.write(f"dbscan_min_sample = {dbscan_min_sample}\n")
     file.write(f"learning_rate = {learning_rate}\n")
-    file.write(f"num_iterations = {num_iterations}\n")
-    file.write(f"prior_circle_dis = {prior_circle_dis}\n")
+    file.write(f"epsilon = {epsilon}\n")
+    file.write(f"prior_circle_r = {prior_circle_r}\n")
     
 def intersection_point(m1, b1, m2, b2):
     coefficients_matrix = np.array([[m1, -1], [m2, -1]])
@@ -77,7 +87,6 @@ def intersection_point(m1, b1, m2, b2):
     return solution[0], solution[1]
 
 def calculate_angle_bisector_slope(m1, m2):
-    # bisector_slope = (m1 * m2 + np.sqrt(m1 * m1 * m2 * m2 + m1 * m1 + m2 * m2 + 1) - 1) / (m1 + m2)
     bisector_slope = (m1 * m2 - np.sqrt(m1 * m1 * m2 * m2 + m1 * m1 + m2 * m2 + 1) - 1) / (m1 + m2)
     return bisector_slope
 
@@ -106,6 +115,14 @@ def calculate_distance(x1, y1, x2, y2, device = "numpy"):
         return np.sqrt((x1 - x2) ** 2 +  (y1 - y2) ** 2)
     elif device == "torch":
         return torch.sqrt((x1 - x2) ** 2 +  (y1 - y2) ** 2) 
+    
+def calculate_cross_angle(x1, y1, x2, y2):
+    vector_a = np.array([x1, y1])
+    vector_b = np.array([x2, y2])
+    cos_theta = np.dot(vector_a, vector_b) / (np.linalg.norm(vector_a) * np.linalg.norm(vector_b))
+    theta_radians = np.arccos(cos_theta)
+    theta_degrees = np.degrees(theta_radians)
+    return theta_degrees
 
 def solve(interval_id, solve_data):
     store_this_interval_path = os.path.join(store_pic_folder_name, 'batch_' + str(interval_id))
@@ -133,27 +150,11 @@ def solve(interval_id, solve_data):
     sorted_indices = np.argsort(downsampled_data[:, 0])
     downsampled_data = downsampled_data[sorted_indices]
     
-    plt.scatter(downsampled_data[:, 0], downsampled_data[:, 1], color='blue', marker='o', label='Scatter Plot', s=10)
-    plt.title('Scatter Plot')
-    plt.xlabel('X-axis')
-    plt.ylabel('Y-axis')
-    plt.legend()
-    plt.savefig(os.path.join(store_this_interval_path, 'scatter_plot.png'))
-    plt.clf()
-    
     nor_vecs = []
     for i in range(len(downsampled_data) - line_fitting_window_size + 1):
         select_points = downsampled_data[i: i + line_fitting_window_size]
         nor_vecs.append(calculate_normal_vector(i, select_points))
     nor_vecs = np.array(nor_vecs)
-    
-    plt.scatter(nor_vecs[:, 1], nor_vecs[:, 2], color='blue', marker='o', label='Scatter Plot', s=10)
-    plt.title('Scatter Plot')
-    plt.xlabel('X-axis')
-    plt.ylabel('Y-axis')
-    plt.legend()
-    plt.savefig(os.path.join(store_this_interval_path, 'nor_vecs_plot.png'))
-    plt.clf()
     
     X = nor_vecs[:, 1:]
     dbscan = DBSCAN(eps=dbscan_eps, min_samples=dbscan_min_sample)
@@ -171,15 +172,16 @@ def solve(interval_id, solve_data):
     plt.title('DBSCAN Clustering')
     plt.legend()
     plt.savefig(os.path.join(store_this_interval_path, 'DBSCAN_clustering_result.png'))
+    plt.savefig(os.path.join(store_this_interval_path, 'DBSCAN_clustering_result.svg'))
     plt.clf()
     
     filtered_labels = [label for label in labels if label != -1]
     label_counts = Counter(filtered_labels)
     most_common_labels = label_counts.most_common(2)
     assert len(most_common_labels) == 2, "Please adjust hyperparameters to prevent the clustering result from being too bad."
-    # 无法分出两个请调整超参数，如调高 expected_interval_count
     
-    plt.scatter(result[:, 0], result[:, 1], color='blue', marker='o', label='Noise', s=10)
+    plt.scatter(result[:, 0], result[:, 1], color='blue', marker='o', label='Noise', s=20, zorder=0)
+
     plt.axis('equal')
     plt.title('Scatter Plot')
     plt.xlabel('X-axis')
@@ -194,49 +196,50 @@ def solve(interval_id, solve_data):
         cluster_points_id = nor_vecs[labels == most_common_label[0]][:, 0].tolist()
         cluster_points_idx = [int(x) for x in cluster_points_id]
         cluster_points = downsampled_data[cluster_points_idx]
-        plt.scatter(cluster_points[:, 0], cluster_points[:, 1], color = colors[i], marker='o', label = 'Class#' + str(i), s=10)
+        plt.scatter(cluster_points[:, 0], cluster_points[:, 1], color = colors[i], marker='o', label = 'Class#' + str(i), s=20,  zorder=1)
         coefficients = np.polyfit(cluster_points[:, 0], cluster_points[:, 1], 1)
         slope, intercept = coefficients
         ms.append(slope)
         bs.append(intercept)
     
-    cross_point = intersection_point(ms[0], bs[0], ms[1], bs[1])
+    cross_point = intersection_point(ms[0], bs[0], ms[1], bs[1])    
     formatted_point = f'({cross_point[0]:.2f}, {cross_point[1]:.2f})'
-    plt.scatter(cross_point[0], cross_point[1], color='black', marker='o', label='CrossPoint', s=20)
+    plt.scatter(cross_point[0], cross_point[1], color='black', marker='x', label='CrossPoint', s=20,  zorder=6)
     plt.text(cross_point[0], cross_point[1], formatted_point, ha='right', va='bottom')
     
     for i, most_common_label in enumerate(most_common_labels):
-        cluster_points_id = nor_vecs[labels == most_common_label[0]][:, 0].tolist()
-        cluster_points_idx = [int(x) for x in cluster_points_id]
-        cluster_points = downsampled_data[cluster_points_idx]
         x1 = cross_point[0]
-        x2 = min(cluster_points[:, 0])
-        x3 = max(cluster_points[:, 0])
-        if abs(x2 - x1) < abs(x3 - x1):
-            x2 = x3
+        if ms[i] > 0:
+            x2 = min(result[:, 0])
+        else:
+            x2 = max(result[:, 0])
         y1 = ms[i] * x1 + bs[i]
         y2 = ms[i] * x2 + bs[i]
-        plt.plot([x1, x2], [y1, y2], label="LineFit#" + str(i), linestyle = '--', color=line_colors[i])
+        plt.plot([x1, x2], [y1, y2], label="LineFit#" + str(i), linestyle = '--', color=line_colors[i], zorder=5)
     
     angle_bisector_slope = calculate_angle_bisector_slope(ms[0], ms[1])
     angle_bisector_intercept = cross_point[1] - angle_bisector_slope * cross_point[0]
     
     x1 = cross_point[0]
-    x2 = 9
     y1 = angle_bisector_slope * x1 + angle_bisector_intercept
-    y2 = angle_bisector_slope * x2 + angle_bisector_intercept
-    # plt.plot([x1, x2], [y1, y2], label="AngleBisector", linestyle = '--', color='black')
+    y2 = min(result[:, 1])
+    x2 = (y2 - angle_bisector_intercept) / angle_bisector_slope
+    plt.plot([x1, x2], [y1, y2], label="AngleBisector", linestyle = '--', color='black', zorder=5)
     
-    dv_x, dv_y = calculate_direction_vector_from_slope(angle_bisector_slope)    
+    dv_x, dv_y = calculate_direction_vector_from_slope(angle_bisector_slope)
 
-    sgd_dis = torch.tensor([prior_circle_dis], requires_grad=True, dtype=float)
-        
+
+    vec1 = np.array([1, ms[0]])
+    vec2 = np.array([1, ms[1]])
+    cos2theta = np.dot(vec1, vec2) / (np.linalg.norm(vec1) * np.linalg.norm(vec2))
+    sgd_dis = torch.tensor([prior_circle_r / np.sqrt((1 - cos2theta) / 2)], requires_grad=True, dtype=float)
+    
     optimizer = optim.Adam([sgd_dis], lr=learning_rate)
-    num_iter = num_iterations
+    last_loss = None
     losses = []
     best_sed_dis = None
     best_loss = None
-    for _ in range(num_iter):
+    while True:
         optimizer.zero_grad()
         circle_center_x = cross_point[0] + sgd_dis * dv_x
         circle_center_y = cross_point[1] + sgd_dis * dv_y
@@ -245,12 +248,10 @@ def solve(interval_id, solve_data):
         dis = calculate_distance(circle_center_x, circle_center_y, xp1, yp1, "torch")
         loss = torch.tensor(0.0, requires_grad=True)
         cnt = 0
-        # tmp = []
         for data in result:
             if data[0] >= min(xp1, xp2) and data[0] <= max(xp1, xp2):
                 loss1 = calculate_distance(data[0], data[1], circle_center_x, circle_center_y, "torch") - dis
                 loss = loss + loss1 * loss1
-                # tmp.append(float(loss1))
                 cnt = cnt + 1
         loss = loss / cnt
         detached_loss = float(loss.detach())
@@ -260,20 +261,16 @@ def solve(interval_id, solve_data):
             best_sed_dis = float(sgd_dis.detach())
         loss.backward()
         optimizer.step()
-        # print("loss: ", float(loss.detach()), "sgd_dis: ", float(sgd_dis.detach().float()), "cnt: ", cnt)
-        # print(tmp, "\n")
-    
-    # sgd_dis = torch.tensor([best_sed_dis], requires_grad=True)
-    
+        if last_loss is not None and abs(last_loss - detached_loss) < epsilon:
+            break
+        last_loss = detached_loss
+        
     circle_center_x = cross_point[0] + best_sed_dis * dv_x
     circle_center_y = cross_point[1] + best_sed_dis * dv_y
     xp1, yp1 = calculate_point_to_line_perpendicular(circle_center_x, circle_center_y, ms[0], bs[0])
     xp2, yp2 = calculate_point_to_line_perpendicular(circle_center_x, circle_center_y, ms[1], bs[1])
     distance = calculate_distance(circle_center_x, circle_center_y, xp1, yp1, "numpy")
-    plt.scatter(circle_center_x, circle_center_y, color='black', marker='o', label='CircleCenter', s=20)
-    plt.scatter(xp1, yp1, color='black', marker='o', label='PerpendicularPoint1', s=20)
-    plt.scatter(xp2, yp2, color='black', marker='o', label='PerpendicularPoint2', s=20)
-    circle = plt.Circle((circle_center_x, circle_center_y), distance , color='black', fill=False)
+    circle = plt.Circle((circle_center_x, circle_center_y), distance , color='black', fill=False, zorder=7)
     plt.gcf().gca().add_artist(circle)
     plt_lightgreen_x = []
     plt_lightgreen_y = []
@@ -281,16 +278,20 @@ def solve(interval_id, solve_data):
         if data[0] >= min(xp1, xp2) and data[0] <= max(xp1, xp2):
             plt_lightgreen_x.append(data[0])
             plt_lightgreen_y.append(data[1])
-    plt.scatter(plt_lightgreen_x, plt_lightgreen_y,  color='lightgreen', marker='o', s=10, label = "TrainPoints")
-            
-    plt.title(f"r = {float(distance)} loss = {float(best_loss):5e}")
-    plt.scatter(xp1, yp1, color='black', marker='o', label='CutPoint1', s=20)
-    plt.scatter(xp2, yp2, color='black', marker='o', label='CutPoint2', s=20)
-    # plt.legend()
+    plt.scatter(plt_lightgreen_x, plt_lightgreen_y,  color='lightgreen', marker='o', s=20, label = "TrainPoints", zorder=4)
+    
+    angle = calculate_cross_angle(xp1 - circle_center_x, yp1 - circle_center_y, xp2 - circle_center_x, yp2 - circle_center_y)
+    
+    plt.title(f"r = {float(distance):5e} loss = {float(best_loss):5e} angle = {float(angle):.2f}°")
+    plt.scatter(circle_center_x, circle_center_y, color='black', marker='o', label='CircleCenter', s=20)
+    plt.scatter(xp1, yp1, color='black', marker='^', label='CutPoint', s=20, zorder=6)
+    plt.scatter(xp2, yp2, color='black', marker='^', s=20, zorder=6)
+    plt.legend(bbox_to_anchor=(1, 1), loc='upper right', fontsize='small')
     plt.savefig(os.path.join(store_this_interval_path, 'new_scatter_plot.png'))
+    plt.savefig(os.path.join(store_this_interval_path, 'new_scatter_plot.svg'))
     plt.clf()
     
-    return distance
+    return distance, angle
 
 example_csv_path = os.path.join("./data", corner_example_name + ".csv")
 example_npy_path = os.path.join("./data", corner_example_name + ".npy")
@@ -310,29 +311,40 @@ except:
 
 unique_values, counts = np.unique(data_array[:, 1], return_counts=True)
 interval_id = 0
-estimated_R = []
-# max_test_count = 1
+estimated_Rs = []
+estimated_angles = []
+
 for i in tqdm(range(0, len(unique_values), expected_interval_count), desc="Processing"):
     interval_id += 1
-    # print("------------[the " + str(interval_id) + "th interval]------------")
     idx = np.where(np.isin(data_array[:, 1], unique_values[i:i + expected_interval_count]))
     solve_data = data_array[idx][:, [0, 1, 2]]
-    estimated_R.append(solve(interval_id, solve_data))
-    # if interval_id >= max_test_count:
-    #     break
+    R, angle = solve(interval_id, solve_data)
+    estimated_Rs.append(R)
+    estimated_angles.append(angle)
 
 csv_file_path = os.path.join(store_result_folder_name, "results.csv")
 with open(csv_file_path, 'w', newline='') as csvfile:
     csv_writer = csv.writer(csvfile)
-    csv_writer.writerow(['Block', 'Values'])  # 写入列名
-    for epoch, value in enumerate(estimated_R, start=1):
-        csv_writer.writerow([epoch, value])
+    csv_writer.writerow(['Block', 'R', 'Angle'])
+    for epoch in range(len(estimated_Rs)):
+        csv_writer.writerow([epoch + 1, estimated_Rs[epoch], estimated_angles[epoch]])
+        
 print(f"results has been saved in the CSV file '{csv_file_path}' successfully!")
 
-plt.hist(estimated_R, bins=20, density=True, alpha=0.7, color='blue')
-plt.title('Histogram of Partial Normal Distribution')
+values = np.array(estimated_Rs)
+iqr = np.percentile(values, 75) - np.percentile(values, 25)
+bin_width_fd = 2 * iqr / (len(values) ** (1/3))
+bins_fd = int((np.max(values) - np.min(values)) / bin_width_fd)
+ax = sns.histplot(values, bins=bins_fd, kde=True, color='blue', edgecolor='black')
+for rect in ax.patches:
+    height = rect.get_height()
+    ax.text(rect.get_x() + rect.get_width() / 2, height, f'{int(height)}', ha='center', va='bottom')
+bin_edges = np.histogram_bin_edges(values, bins=bins_fd)
+plt.xticks(bin_edges)
+plt.title('Histogram of Values (Freedman-Diaconis Rule)')
+plt.xticks(rotation=45)
 plt.xlabel('Values')
 plt.ylabel('Frequency')
-plt.savefig(os.path.join(store_result_folder_name, "results_box.png"))
-plt.show()
+plt.savefig(os.path.join(store_result_folder_name, "results_pic.png"))
+plt.savefig(os.path.join(store_result_folder_name, "results_pic.svg"))
 plt.clf()
